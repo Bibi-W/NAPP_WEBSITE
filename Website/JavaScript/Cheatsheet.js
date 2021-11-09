0017ca1dc572b758be228295bf0bc5a7b6258683
WyattMelt0n <90633932+WyattMelt0n@users.noreply.github.com>
2021-11-08 20:47:03 -0600

    Add files via upload

--- Website/JavaScript/Cheatsheet.js
+++ Website/JavaScript/Cheatsheet.js
@@ -2,7 +2,6 @@ function do_calc(){
   var num = Number(document.getElementById("in").value);
 
   if(num >= 0) {
-    document.getElementById("res1").innerHTML = num + "cal/lb";
     document.getElementById("res2").innerHTML = Math.round(num * 0.02) + "g";
     document.getElementById("res3").innerHTML = Math.round(num * 0.005) + "g";
     document.getElementById("res4").innerHTML = 0 + "g";
